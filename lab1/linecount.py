d7d53012f1380f453aca15f86e19d909e058c020
Reza Mohamed <mohamedr@my.lanecc.edu>
2024-06-05 10:19:08 -0700

    updated lab1

--- lab1/linecount.py
+++ lab1/linecount.py
@@ -1,3 +1,17 @@
+import sys
 
-print("hello world")
+def count_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            return len(lines)
+    except FileNotFoundError:
+        return "File not found. Please check the filename and try again."
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python linecount.py <filename>")
+    else:
+        filename = sys.argv[1]
+        line_count = count_lines(filename)
+        print(f"The file {filename} has {line_count} lines.")
